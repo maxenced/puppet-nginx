6f30d444b51160ac79eda2f3caf26a8ea7065bcd
Maxence Dunnewind <maxence@typhon.com>
2019-01-16 11:49:54 +0100

    remove ensure parameter from concat::fragment as it is deprecated and raise log lines in puppetserser

--- manifests/resource/vhost.pp
+++ manifests/resource/vhost.pp
@@ -566,7 +566,6 @@ define nginx::resource::vhost (
 
   if ($listen_port != $ssl_port) {
     concat::fragment { "${name_sanitized}-header":
-      ensure  => present,
       target  => $config_file,
       content => template('nginx/vhost/vhost_header.erb'),
       order   => '001',
@@ -576,7 +575,6 @@ define nginx::resource::vhost (
   # Create a proper file close stub.
   if ($listen_port != $ssl_port) {
     concat::fragment { "${name_sanitized}-footer":
-      ensure  => present,
       target  => $config_file,
       content => template('nginx/vhost/vhost_footer.erb'),
       order   => '699',
